c12631f6f82e540023e7c85d8dc8a6cd4f8aef59
Nadia Mughal <nmughal14@gmail.com>
2017-04-12 16:49:36 -0400

    Removed extra spacing

--- server/routes/job.routes.js
+++ server/routes/job.routes.js
@@ -1,40 +1,20 @@
 const jobRouter = require('express').Router();
 
 let allJobs = [
-  {
-  'id': '478485',
-  'company': 'Iron Yard',
-  'link': 'http://www.theironyard.com',
-  'notes': 'Coding school',
-  'createTime': '4/14/17'
-  },
-  {
-  'id': '467365',
-  'company': 'General Assembly',
-  'link': 'http://www.generalassembly.com',
-  'notes': 'Coding school',
-  'createTime': '4/15/17'
-  },
-  {
-  'id': '85785',
-  'company': 'Coding Dojo',
-  'link': 'http://www.codingdojo.com',
-  'notes': 'Coding school',
-  'createTime': '4/13/17'
-  }
+  {'id': '478485', 'company': 'Iron Yard', 'link': 'http://www.theironyard.com', 'notes': 'Coding school', 'createTime': '4/14/17'},
+  {'id': '467365', 'company': 'General Assembly', 'link': 'http://www.generalassembly.com', 'notes': 'Coding school', 'createTime': '4/15/17'},
+  {'id': '85785', 'company': 'Coding Dojo', 'link': 'http://www.codingdojo.com', 'notes': 'Coding school', 'createTime': '4/13/17'}
 ];
 
 jobRouter.get('/', function showAllJobs(request, response, next){
   let newJobArray = [];
 
   allJobs.forEach(function getJobInfo(job){
-    console.log(job.id, job.company, job.link);
-
     newJobArray.push({'id': job.id, 'company':job.company, 'link': job.link});
-
   });
 
   response.json(newJobArray);
+
 });
 
 function addAJob(request, response, next){
